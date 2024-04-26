7f93be7151b25c96b2735a068c98174223235e11
Aryan2002Vedy <157814531+Aryan2002Vedy@users.noreply.github.com>
2024-04-26 14:12:42 +0530

    CORS UPDATED index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(cors({
-    origin:["http://localhost:3000","http://localhost:3001"],
+    origin:["http://localhost:3000","http://localhost:3001","https://vedy-task-manager.netlify.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }));
