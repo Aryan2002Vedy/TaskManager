344fa3afbfd4496337e0e1c67b7ce1068d6a217c
Aryan2002Vedy <157814531+Aryan2002Vedy@users.noreply.github.com>
2024-04-28 16:47:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,11 +17,11 @@ dbConnection()
 const PORT = process.env.PORT || 5000
 
 const app = express()
-
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.use(express.json());
+
 
 app.use(cookieParser());
 app.use(morgan("dev"));
